c6f761331b5ea92ae2b300c9091d26b9fb128162
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-07-03 22:59:28 +0000

    jdbc.propertiesによるDB接続追加

--- README.md
+++ README.md
@@ -10,6 +10,17 @@ mvn archetype:generate \
     -DinteractiveMode=false
 ```
 
+## Config
+
+_src/main/resources/jdbc.properties_
+
+```properties
+# e.g.)
+jdbc.url=jdbc:mysql://<HOST>:<PORT>/<DATABASE>?useSSL=false&serverTimezone=Asia/Tokyo&useUnicode=true&characterEncoding=utf8
+jdbc.username=<USER>
+jdbc.password=<PASS>
+```
+
 ## Build
 
 ```sh
@@ -21,4 +32,10 @@ mvn clean package
 ```sh
 java -jar target/mysql-connector-java-check-1.0-SNAPSHOT.jar
 # java -cp target/mysql-connector-java-check-1.0-SNAPSHOT.jar jp.co.opentone.App
+```
+
+## Test
+
+```sh
+mvn test
 ```
--- src/main/java/jp/co/opentone/App.java
+++ src/main/java/jp/co/opentone/App.java
@@ -1,11 +1,58 @@
 package jp.co.opentone;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // JDBC URL, username and password of MySQL server
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String jdbcUrl = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+
+        // JDBC variables for opening and managing connection
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            // Opening database connection
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // Creating statement object
+            statement = connection.createStatement();
+
+            // Executing SQL query and getting the result
+            String query = "SELECT NOW()";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // Processing the result
+            while (resultSet.next()) {
+                String result = resultSet.getString(1);
+                System.out.println("Query Result: " + result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Closing connection
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
